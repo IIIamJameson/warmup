ccce05d5c31ab6aed31d5b4bce7b77179d372dc7
IIIamJameson <ydavidovskaya9@gmail.com>
2018-02-22 22:45:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 module.exports = function warmup(temperature) {
   // your implementation here
-if (temperature === -20) return temperature*1.8+32;
- if (temperature === 0) return temperature*1.8+32;
- if (temperature === 15) return temperature*1.8+32;
- if (temperature === 40) return temperature*1.8+32;
+if (temperature == -20||0||15||40) return temperature*1.8+32;
 };
